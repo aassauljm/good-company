2f1501f912d645d0d0de9e26afb58ec2f6a3f4a3
Joshua Gagnon <josh.n.gagnon@gmail.com>
2015-10-20 12:56:30 +1300

    Minor clean up of user fixture

--- config/fixtures/user.js
+++ config/fixtures/user.js
@@ -3,15 +3,6 @@
  * @param adminRole - the admin role which grants all permissions
  */
 exports.create = function (roles, userModel) {
-  if (_.isEmpty(sails.config.permissions.adminUsername)) {
-    throw new Error('sails.config.permissions.adminUsername is not set');
-  }
-  if (_.isEmpty(sails.config.permissions.adminPassword)) {
-    throw new Error('sails.config.permissions.adminPassword is not set');
-  }
-  if (_.isEmpty(sails.config.permissions.adminEmail)) {
-    throw new Error('sails.config.permissions.adminEmail is not set');
-  }
   return User.findOne({ username: sails.config.permissions.adminUsername })
     .then(function (user) {
       if (user) return user;
