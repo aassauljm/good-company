2a702e5ecdfe4fa56fe289c408c4d4dde37bc1a0
Joshua Gagnon <josh.n.gagnon@gmail.com>
2015-12-09 08:36:15 +1300

    rendering basic share register

--- assets/js/components/company.js
+++ assets/js/components/company.js
@@ -332,8 +332,48 @@ export class CompanyDetails extends React.Component {
     }
 }
 
-{
+function renderShareClass(shareClass){
+    return shareClass || 'Ordinary'
+}
 
+
+function renderIssue(action){
+    const date = new Date(action.effectiveDate).toDateString();
+    return `${action.data.amount} ${renderShareClass(action.data.shareClass)} on ${date}`
+}
+
+function renderTransferTo(action){
+    const date = new Date(action.effectiveDate).toDateString();
+    return  `${action.data.amount} ${renderShareClass(action.data.shareClass)} on ${date}`
+}
+
+function renderTransferFrom(action){
+    const date = new Date(action.effectiveDate).toDateString();
+    return  `${action.data.afterAmount} ${renderShareClass(action.data.shareClass)} on ${date}`
+}
+
+class RenderActions extends React.Component {
+
+    renderAction(action) {
+        switch(action.type){
+            case 'ISSUE_TO':
+                return renderIssue(action);
+            case 'TRANSFER_TO':
+                return renderTransferTo(action);
+            case 'TRANSFER_FROM':
+                return renderTransferFrom(action);
+            default:
+                return false;
+        }
+    };
+
+    render() {
+        return <ul>
+            { this.props.actions.map((action, i) => {
+                return <li key={i}> { this.renderAction(action) }</li>
+            })}
+        </ul>
+    };
 }
 
 
@@ -349,7 +389,7 @@ export class ShareRegister extends React.Component {
         return this.props.params.id
     };
 
-    fetch() {
+fetch() {
         return this.props.dispatch(requestResource('/company/'+this.key()+'/share_register'))
     };
 
@@ -361,6 +401,20 @@ export class ShareRegister extends React.Component {
         this.fetch();
     };
 
+    renderField(key, data) {
+        if(Array.isArray(data)){
+            return  <RenderActions actions={data}/>
+        }
+        switch(key){
+            case 'shareClass':
+                return renderShareClass(data);
+
+            default:
+                return data;
+        }
+
+    }
+
     renderTable(shareRegister) {
         return <table className="table table-responsive">
             <thead>
@@ -371,7 +425,7 @@ export class ShareRegister extends React.Component {
             <tbody>
                 { shareRegister.map((s, i) => {
                     return <tr key={i}>{ this.fields.map((f, j) => {
-                        return <td key={j}>{ Array.isArray(shareRegister[i][f]) ? JSON.stringify(shareRegister[i][f]) : shareRegister[i][f]  }</td>
+                        return <td key={j}>{this.renderField(f, shareRegister[i][f])}</td>
                     })}</tr>
                 }) }
             </tbody>
