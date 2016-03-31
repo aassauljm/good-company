f3538936a12353ad77498b6b834793909e1bf574
Joshua Gagnon <josh.n.gagnon@gmail.com>
2016-03-31 16:44:00 +1300

    added subdivision file

---
+++ assets/js/components/transactions/subdivision.js
@@ -0,0 +1,127 @@
+"use strict";
+import React, {PropTypes} from 'react';
+import Modal from 'react-bootstrap/lib/Modal';
+import Button from 'react-bootstrap/lib/Button';
+import { connect } from 'react-redux';
+import { reduxForm } from 'redux-form';
+import Input from '../forms/input';
+import STRINGS from '../../strings'
+import { numberWithCommas } from '../../utils'
+import Glyphicon from 'react-bootstrap/lib/Glyphicon';
+import { fieldStyle, fieldHelp } from '../../utils';
+import { Link } from 'react-router';
+import { companyTransaction, addNotification } from '../../actions';
+
+
+function renderHolders(holding){
+    return <ul>
+        { holding.holders.map((h, i) => {
+            return <li key={i}>{ h.name } </li>
+        })}
+    </ul>
+}
+
+function renderAmount(holding){
+    return <ul>
+        { holding.parcels.map((h, i) => {
+            return <li key={i}>{ numberWithCommas(h.amount) } </li>
+        })}
+    </ul>
+}
+
+
+
+export class Subdivide extends React.Component {
+    static propTypes = {
+        options: PropTypes.array.isRequired,
+    };
+    renderSelect(shareClass) {
+        return <Input type="select" {...shareClass} bsStyle={fieldStyle(shareClass)} help={fieldHelp(shareClass)}
+            hasFeedback >
+                <option></option>
+                { this.props.options }
+            </Input>
+    }
+
+    render() {
+        return
+    }
+}
+
+
+
+@connect(undefined)
+export class SubdivisionModal extends React.Component {
+    constructor(props) {
+        super(props);
+         this.submit = ::this.submit;
+    }
+
+    handleNext() {
+       // this.refs.form.submit();
+       this.submit();
+    }
+
+    submit(values) {
+        this.props.end({reload: true});
+        const holdings = [];
+        Object.keys(values).map(k => {
+            if(!values[k]) return;
+            holdings.push({
+                holdingId: parseInt(k, 10),
+                shareClass: parseInt(values[k], 10),
+                transactionType: 'SUBDIVISION'
+            });
+        });
+        this.props.dispatch(companyTransaction('compound',
+                                this.props.modalData.companyId,
+                                {actions: holdings}))
+            .then(() => {
+                this.props.end({reload: true});
+                this.props.dispatch(addNotification({message: 'Share subdivided.'}));
+                const key = this.props.modalData.companyId;
+            })
+            .catch((err) => {
+                this.props.dispatch(addNotification({message: err.message, error: true}));
+            });
+    }
+
+    renderBody(companyState) {
+        const shareClasses = ((companyState.shareClasses || {}).shareClasses || []);
+
+        const options = shareClasses.map((s, i) => {
+            return <option key={i} value={s.id}>{s.name}</option>
+        })
+        return <table className="table table-striped">
+            <thead>
+            <tr><th>Name</th><th>Shareholders</th><th>Shares</th><th>Share Class</th></tr>
+            </thead>
+            <tbody>
+                { companyState.holdingList.holdings.map((h, i) => {
+                    return <tr key={i}>
+                        <td>{ h.name }</td>
+                        <td>{ renderHolders(h) }</td>
+                        <td>{ renderAmount(h) }</td>
+                        <td></td>
+                    </tr>
+                })}
+            </tbody>
+        </table>
+    }
+
+    render() {
+        return  <Modal ref="modal" show={true} bsSize="large" onHide={this.props.end} backdrop={'static'}>
+              <Modal.Header closeButton>
+                <Modal.Title>Subdivide Shares</Modal.Title>
+              </Modal.Header>
+              <Modal.Body>
+               THIS FUNCTIONALITY IS UNDER DEVELOPMENT
+                { this.renderBody(this.props.modalData.companyState) }
+              </Modal.Body>
+              <Modal.Footer>
+                <Button onClick={this.props.end} >Close</Button>
+                 <Button onClick={::this.handleNext} bsStyle="primary">{ 'Submit' }</Button>
+              </Modal.Footer>
+            </Modal>
+    }
+}
