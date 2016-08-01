6ecf719267d6eafd5096e448a749fb59614cedc9
Joshua Gagnon <josh.n.gagnon@gmail.com>
2016-08-02 11:55:31 +1200

    added catalex oauth2 strat

---
+++ api/services/protocols/catalex.js
@@ -0,0 +1,18 @@
+// api/services/protocols/oauth2.js
+
+var _ = require('lodash');
+
+
+module.exports = function (req, profile, next) {
+      var query = {
+        identifier: profile.id.toString(),
+        protocol: 'oauth2',
+        //tokens: { accessToken: accessToken }
+      };
+
+      /*if (!_.isUndefined(refreshToken)) {
+        query.tokens.refreshToken = refreshToken;
+      }*/
+    profile.username = profile.name;
+    return sails.services.passport.connect(req, query, profile, next);
+}
