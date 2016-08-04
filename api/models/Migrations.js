9aebaa302b0eee4b38f00fbcb9cf759eb32c4d47
Joshua Gagnon <josh.n.gagnon@gmail.com>
2016-08-04 17:13:57 +1200

    Added migration

---
+++ api/models/Migrations.js
@@ -0,0 +1,22 @@
+// api/models/Model.js
+
+var _ = require('lodash');
+
+
+
+module.exports = {
+
+
+    attributes: {
+		name: {
+			type: Sequelize.TEXT
+		}
+    },
+    options: {
+        tableName: 'migrations',
+        classMethods: {},
+        instanceMethods: {},
+        hooks: {},
+        timestamps: false,
+    }
+};
---
+++ migrations/1470272903_add_director_data.sql
@@ -0,0 +1,1 @@
+ALTER TABLE director add column data JSON;
