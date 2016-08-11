59f3ab2bff3c56f8c3379651ad560103c916d013
Joshua Gagnon <josh.n.gagnon@gmail.com>
2016-08-11 18:58:48 +1200

    tweaked update script

--- update.sh
+++ update.sh
@@ -1,13 +1,9 @@
 #!/usr/bin/env bash
+#pm2 start app.js -f -n good_company -x -- --prod
+
 pm2 stop good-company
-pm2 start maintainence.js --name gc-maintainence
-
-sudo -u $1 git pull
-
-sudo -u $1 script
-
-sudo -u $1 NODE_ENV=production webpack
-
-pm2 stop gc-maintainence
-
-pm2 restart good-company
+pm2 start maintanence.js --name gc-maintainence
+git pull
+NODE_ENV=production webpack
+pm2 stop gc-maintanence
+pm2 start app.js -f -n good_company -x -- --prod
