995855e96fda696be701e2ac3c2827e7f9a273c5
Joshua Gagnon <josh.n.gagnon@gmail.com>
2016-08-17 14:00:35 +1200

    fixed migration again

--- migrations/1471301559_rename_holder_j.sql
+++ migrations/1471301559_rename_holder_j.sql
@@ -1,5 +1,5 @@
 ALTER TABLE "holderJ" rename TO "holder";
 
-ALTER TABLE "holder" DROP CONSTRAINT holder_j_pkey;
+ALTER TABLE "holder" DROP CONSTRAINT "holderJ_pkey";
 
 ALTER TABLE "holder" ADD COLUMN id BIGSERIAL PRIMARY KEY;
