3f99a595115b87b05c5ba21a001619ad8ecc4eac
Joshua Gagnon <josh.n.gagnon@gmail.com>
2016-08-30 10:34:40 +1200

    made update script less stupid

--- update.sh
+++ update.sh
@@ -2,10 +2,10 @@
 #pm2 start app.js -f -n good_company -x -- --prod
 
 rm .tmp/public/*/*
+git pull
 pm2 stop good_company
 pm2 start maintenance.js --name gc_maintenance
 node scripts/migrate.js config/env/production.js
-git pull
 NODE_ENV=production webpack
 pm2 stop gc_maintenance
 pm2 start app.js -f -n good_company -x -- --prod
