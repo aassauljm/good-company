f6e2e4d7d9d6d19165258f78794cc34d2d42d5ea
Joshua Gagnon <josh.n.gagnon@gmail.com>
2016-09-04 14:10:56 +1200

    fixed bug with not recognizing an annual return

--- api/models/CompanyState.js
+++ api/models/CompanyState.js
@@ -487,7 +487,8 @@ module.exports = {
                     const humanDiff = due.clone().from(moment());
                     const thisYear = (new Date()).getFullYear();
                     const ar = _.find(documents, d => {
-                        return d.type === 'Companies Office' && d.filename === 'Online Annual Return' && d.date.getFullYear() === thisYear;
+                        console.log(d, d.date.getFullYear(), thisYear)
+                        return d.type === 'Companies Office' && (d.filename === 'Online Annual Return' || d.filename === 'Annual Return Filed') && d.date.getFullYear() === thisYear;
                     });
                     if(!ar){
                         if(diff > 0){
