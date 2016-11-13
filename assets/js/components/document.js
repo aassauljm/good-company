6d188c902700e37db8462c20090ce0c16e842443
Joshua Gagnon <josh.n.gagnon@gmail.com>
2016-11-14 11:54:18 +1300

    updated document view

--- assets/js/components/document.js
+++ assets/js/components/document.js
@@ -6,6 +6,8 @@ import { connect } from 'react-redux';
 import { initialize } from 'redux-form';
 import { Link } from 'react-router';
 import { fieldStyle } from '../utils';
+import AutoAffix from 'react-overlays/lib/AutoAffix'
+
 
 @connect((state, ownProps) => state.resources['/document/'+ownProps.params.id ] || {data: {}})
 export default class Document extends React.Component {
@@ -26,30 +28,44 @@ export default class Document extends React.Component {
         return this.key() === this.props.id+'';
     }
 
-    render() {
-        const data = this.props.data || {};
+    renderDetails() {
+        return <div className={"widget"}>
+                <div className="widget-header">
+                    <div className="widget-title">
+                        File Details
+                    </div>
+                </div>
+                <div className="widget-body">
+                     <dl>
+                          <dt>ID</dt>
+                          <dd>{data.id}</dd>
+                          <dt>Filename</dt>
+                          <dd>{data.filename}</dd>
+                          <dt>Type</dt>
+                          <dd>{data.type}</dd>
+                          { data.date && <dt>Date</dt> }
+                          { data.date && <dd>{ new Date(data.date).toDateString() }</dd> }
+                          <dt>Date Imported</dt>
+                          <dd>{stringToDate(data.createdAt)}</dd>
+                          { data.sourceUrl && <dt>Original URL</dt> }
+                          { data.sourceUrl && <dd><Link target="_blank" to={data.sourceUrl}>Companies Office</Link> </dd> }
+                        </dl>
+                          <div className="button-row">{ !data.sourceUrl && <Link target="_blank" className="btn btn-primary" to={`/api/document/get_document/${this.key()}`}>Download</Link> }</div>
+                </div>
+        </div>
+    }
+    render(){
         return <div className="container">
-            <div className="col-md-12">
-                 <div className="button-row">{ !data.sourceUrl && <Link target="_blank" className="btn btn-primary" to={`/api/document/get_document/${this.key()}`}>Download</Link> }</div>
-            </div>
-            <div className="col-md-9">
-                <img className="image-loading" src={"/api/document/get_document_preview/"+ this.key()} />
-            </div>
-             <div className="col-md-3">
-             <dl>
-                  <dt>ID</dt>
-                  <dd>{data.id}</dd>
-                  <dt>Filename</dt>
-                  <dd>{data.filename}</dd>
-                  <dt>Type</dt>
-                  <dd>{data.type}</dd>
-                  <dt>Date</dt>
-                  <dd>{new Date(data.date).toDateString()}</dd>
-                  <dt>Date Imported</dt>
-                  <dd>{stringToDate(data.createdAt)}</dd>
-                  { data.sourceUrl && <dt>Original URL</dt> }
-                  { data.sourceUrl && <dd><Link target="_blank" to={data.sourceUrl}>Companies Office</Link> </dd> }
-                </dl>
+            <div className="row" ref="affixContainer">
+                <div className="col-md-12 col-lg-3 col-lg-push-9">
+                    <AutoAffix viewportOffsetTop={15} container={() => this.refs.affixContainer}>
+                        { this.renderDetails() }
+                    </AutoAffix>
+                </div>
+                <div className="col-md-12 col-lg-9 col-lg-pull-3">
+                     <img className="image-loading" src={"/api/document/get_document_preview/"+ this.key()} />
+
+                </div>
             </div>
         </div>
     }
