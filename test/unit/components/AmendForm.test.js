0544bc54d37c6074f3f19e571cd4013a8245794b
Joshua Gagnon <josh.n.gagnon@gmail.com>
2017-01-20 19:08:56 +1300

    update amend test to handle new grouping strat

--- test/unit/components/AmendForm.test.js
+++ test/unit/components/AmendForm.test.js
@@ -18,10 +18,12 @@ describe('Amend submit', () => {
                 const results = formatSubmit(data.values, data.actionSet);
 
                 results.length.should.be.equal(6);
+
                 results[0].data.actions.length.should.be.equal(7);
-                results[1].data.actions.length.should.be.equal(1);
-                results[1].data.actions[0].transactionType.should.be.equal('ISSUE_TO');
-                results[2].data.actions.length.should.be.equal(2);
+                results[1].data.actions.length.should.be.equal(2);
+                results[1].data.actions[0].transactionType.should.be.equal('TRANSFER_TO');
+                results[2].data.actions[0].transactionType.should.be.equal('ISSUE_TO');
+                results[2].data.actions.length.should.be.equal(1);
                 results[1].data.effectiveDate.should.not.equal(results[2].data.effectiveDate);
                 let lastDate = new Date(results[0].data.effectiveDate)
                 for(var i=1;i<results.length;i++){
@@ -50,7 +52,7 @@ describe('Amend submit', () => {
                 })
                 const results = formatSubmit(data.values, data.actionSet);
 
-                results.length.should.be.equal(8);
+                results.length.should.be.equal(7);
                 let lastDate = new Date(results[0].data.effectiveDate)
                 for(var i=1;i<results.length;i++){
                     const thisDate = new Date(results[i].data.effectiveDate);
@@ -62,6 +64,12 @@ describe('Amend submit', () => {
                         return sum + (a.transactionMethod === 'NEW_ALLOCATION' ? 1 : 0)
                     }, sum)
                 }, 0).should.be.equal(5);
+                results.reduce((sum, r) => {
+                    return r.data.actions.reduce((sum, a) => {
+                        return sum + (a.transactionType === 'ISSUE_TO' ? 1 : 0)
+                    }, sum)
+                }, 0).should.be.equal(2);
+
                 done();
             })
             .catch(done);
