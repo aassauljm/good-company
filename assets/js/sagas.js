e1203cbe6f896997d86f5f65cbb4ff46abc8a2ea
Joshua Gagnon <josh.n.gagnon@gmail.com>
2017-02-27 12:37:04 +1300

    Added sagas

---
+++ assets/js/sagas.js
@@ -0,0 +1,38 @@
+import { delay } from 'redux-saga'
+import { put, call, takeEvery, takeLatest } from 'redux-saga/effects'
+import { fetch } from './utils';
+import { checkStatus, parse } from './middleware'
+
+import { LOOKUP_COMPANY_CHANGE, LOOKUP_COMPANY_REQUEST, LOOKUP_COMPANY_SUCCESS, LOOKUP_COMPANY_FAILURE } from './actionTypes'
+
+const fetchAndProcess = (...args) => fetch(...args).then(checkStatus).then(parse);
+
+
+export function* lookupCompanyOnChange() {
+    yield takeLatest(LOOKUP_COMPANY_CHANGE, fetchLookupCompany);
+}
+
+
+function* fetchLookupCompany(action) {
+    yield put({ type: LOOKUP_COMPANY_REQUEST, payload: action.payload.query });
+    yield call(delay, 300)
+    try {
+        const result = yield fetchAndProcess('/api/company/lookup?query=' + encodeURIComponent(action.payload.query), {
+                credentials: 'same-origin'
+            });
+        yield put({ type: LOOKUP_COMPANY_SUCCESS, response: result })
+    }
+    catch(err) {
+        yield put({ type: LOOKUP_COMPANY_FAILURE , err })
+    }
+}
+
+
+export function* rootSagas(){
+    yield [lookupCompanyOnChange()];
+}
+
+
+export function runSagas(sagaMiddleware){
+    sagaMiddleware.run(rootSagas);
+}
