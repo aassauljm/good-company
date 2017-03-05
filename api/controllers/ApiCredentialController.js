e49a763628aa02e8a85dc3d0515f5a1aca5f00ee
Paddy Moran <pdmoran@outlook.com>
2017-03-06 11:08:41 +1300

    Finished basics of authorisedCompanies route

--- api/controllers/ApiCredentialController.js
+++ api/controllers/ApiCredentialController.js
@@ -7,6 +7,20 @@ import fetch from "isomorphic-fetch";
  * @help        :: See http://links.sailsjs.org/docs/controllers
  */
 
+const nzbnServiceToken = '9f107b532c6b0869e2dd74876ff3a001';
+
+function getUserMbieAccessToken(user) {
+    return user.getApiCredentials({
+        where: {
+            service: 'nzbn'
+        },
+        order: [
+            ['createdAt', 'DESC']
+        ]
+    })
+    .then(result => result ? result[0].dataValues.accessToken : null);
+}
+
 module.exports = {
 
     mbie: function (req, res) {
@@ -19,22 +33,35 @@ module.exports = {
     },
 
     authorisedCompanies: function (req, res) {
-        fetch('https://sandbox.api.business.govt.nz/services/v3/nzbn/users', {
-                headers: {
-                    Accept: 'application/json',
-                    'NZBN-Authorization': 'Bearer d3f7b60c-f687-4291-92ed-18a3fe0c278f',
-                    Authorization: 'Bearer 09cd500d647a54069ad85cc6b23b019a'
+        getUserMbieAccessToken(req.user)
+            .then(nzbnUserAccessToken => {
+                if (!nzbnUserAccessToken) {
+                    return res.json({message: ['No MBIE access token']});
                 }
+
+                const headers = {
+                    Accept: 'application/json',
+                    'NZBN-Authorization': 'Bearer ' + nzbnUserAccessToken,
+                    Authorization: 'Bearer ' + nzbnServiceToken
+                };
+
+                return fetch('https://sandbox.api.business.govt.nz/services/v3/nzbn/users', {headers})
+                    .then(response => response.json())
+                    .then(users => users.users[0].userId)
+                    .then(userNzbnId => {
+                        return fetch('https://sandbox.api.business.govt.nz/services/v3/nzbn/authorities?user-id=' + userNzbnId, {
+                            headers: {
+                                Accept: 'application/json',
+                                'NZBN-Authorization': 'Bearer ' + nzbnUserAccessToken,
+                                Authorization: 'Bearer ' + nzbnServiceToken
+                            }
+                        });
+                    })
+                    .then(response => response.json())
+                    .then(result => {
+                        return res.json(result.items);
+                    });
             })
-            .then(response => {
-                return response.text();
-            })
-            .then(users => {
-                users = JSON.parse(users);
-                // This call returns the current user. There will always be one and only one user - therefore, let's take the first item in the users array
-                const userNzbnId = users[0].userId;
-                res.json(userNzbnId);
-            });
     }
 
 };
