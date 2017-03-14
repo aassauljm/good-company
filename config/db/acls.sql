1e65b537b29dda22e96dd3983e4c9d75186bfc84
Joshua Gagnon <josh.n.gagnon@gmail.com>
2017-03-15 11:06:26 +1300

    more wip

--- config/db/acls.sql
+++ config/db/acls.sql
@@ -19,13 +19,21 @@ END
 $func$ LANGUAGE plpgsql;
 
 
-CREATE OR REPLACE FUNCTION get_user(_tbl regclass, id integer, OUT result integer) AS
+CREATE OR REPLACE FUNCTION get_owner(_tbl regclass, id integer, OUT result integer) AS
 $func$
 BEGIN
-EXECUTE format('SELECT "userId" FROM %s WHERE id = %s', $1, $2)
+EXECUTE format('SELECT "ownerId" FROM %s WHERE id = %s', $1, $2)
 INTO result;
 END
 $func$ LANGUAGE plpgsql;
+
+
+CREATE OR REPLACE FUNCTION get_company_owner(_tbl regclass, id integer)
+RETURNS INTEGER
+    AS $$
+    SELECT "ownerId" from company where id = $2;
+$$ LANGUAGE SQL;
+
 
 CREATE OR REPLACE FUNCTION get_org(_tbl regclass, id integer, OUT result integer) AS
 $func$
@@ -43,7 +51,7 @@ CREATE OR REPLACE FUNCTION generate_aces(modelName text,  entityId integer defau
             SELECT action::text as permission,
              CASE
         WHEN p."relation" = 'role' AND $2 IS NULL THEN 'role:' || p."roleId"
-        WHEN  p."relation" = 'organisation' and $2 IS NOT NULL   THEN 'org:' || get_org(m.identity, $2)
+        WHEN  p."relation" = 'organisation' and $2 IS NOT NULL THEN 'org:' || get_org(m.identity, $2)
         WHEN  p."relation" = 'user' and $2 IS NOT NULL   THEN 'id:' || get_user(m.identity, $2)
         WHEN  p."relation" = 'owner' and $2 IS NOT NULL  THEN 'id:' || get_owner(m.identity, $2)
         ELSE null
@@ -56,3 +64,58 @@ CREATE OR REPLACE FUNCTION generate_aces(modelName text,  entityId integer defau
             ORDER BY p."userId" is not null
             ) q WHERE principal IS NOT NULL
 $$ LANGUAGE SQL;
+
+
+CREATE OR REPLACE FUNCTION generate_principals(userId integer)
+    RETURNS SETOF text
+    AS $$
+        SELECT 'id:' || $1
+
+        UNION
+
+        SELECT 'role:' || r.id
+            FROM user_roles ur
+            LEFT OUTER JOIN role r on ur."roleId" = r.id
+            WHERE ur."userId" = $1
+
+        UNION
+
+        SELECT 'org:' || 10
+
+$$ LANGUAGE SQL;
+
+CREATE OR REPLACE FUNCTION get_permissions(userId integer, action text, modelName text, entityId integer default NULL)
+    RETURNS SETOF TEXT
+        AS $$
+
+        WITH principals as (
+            SELECT generate_principals($1) as principal
+        ),
+        aces as (
+            SELECT (a).permission, (a).principal, (a).allow, row_number() OVER () as index FROM generate_aces($3, $4) a
+        )
+        SELECT permission FROM aces a
+        JOIN principals p on a.principal = p.principal
+        WHERE allow = TRUE
+        ORDER BY index
+$$ LANGUAGE SQL;
+
+
+CREATE OR REPLACE FUNCTION check_permission(userId integer, permission text, modelName text, entityId integer default NULL)
+    RETURNS BOOLEAN
+        AS $$
+
+        WITH principals as (
+            SELECT generate_principals($1) as principal
+        ),
+        aces as (
+            SELECT (a).permission, (a).principal, (a).allow, row_number() OVER () as index FROM generate_aces($3, $4) a
+        )
+        SELECT COALESCE((SELECT a.allow
+        FROM aces a
+        JOIN principals p on a.principal = p.principal
+        WHERE permission = $2
+        ORDER BY index
+        LIMIT 1), FALSE)
+$$ LANGUAGE SQL;
+
