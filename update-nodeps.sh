eb378e39292f34049e9da601e28df7a8ed0393ec
Joshua Gagnon <josh.n.gagnon@gmail.com>
2017-03-14 16:31:55 +1300

    added new update script

---
+++ update-nodeps.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+#pm2 start app.js -f -n good_company -x -- --prod
+
+git pull
+
+rm serviceIsLive.flag
+pm2 stop gc.prod.config.json
+node scripts/migrate.js config/env/production.js
+rm .tmp/public/*/*
+NODE_ENV=production webpack
+pm2 start gc.prod.config.json --env production
