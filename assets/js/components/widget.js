57a2fa6351e279b5ec35f911f77866c59ca1b025
Joshua Gagnon <josh.n.gagnon@gmail.com>
2017-05-11 11:27:06 +1200

    new widget styles

--- assets/js/components/widget.js
+++ assets/js/components/widget.js
@@ -6,17 +6,21 @@ import { Link } from 'react-router';
 export const NavWidget = (props) => {
     return <div className={"widget " + (props.className ? props.className : '')}>
              <Link to={props.link} >
-            <span className="widget-header">
-                <span className="widget-title">
-                    <i className={props.iconClass ? props.iconClass : ''}/> { props.title }
+             <span className="widget-header-wrap">
+                <span className="widget-header">
+                    <span className="widget-title">
+                        <i className={props.iconClass ? props.iconClass : ''}/> { props.title }
+                    </span>
+                    <span className="widget-control">
+                        View All
+                    </span>
                 </span>
-                <span className="widget-control">
-                    View All
                 </span>
-            </span>
-                </Link>
+            </Link>
+            <div className="widget-body-wrap">
             <div className={"widget-body " + (props.bodyClass ? props.bodyClass : '')}>
                { props.children }
+            </div>
             </div>
         </div>
 }
@@ -27,13 +31,17 @@ const Widget = (props) => {
         return <NavWidget {...props} />
     }
     return <div className={"widget " + (props.className ? props.className : '')}>
+    <div className="widget-header-wrap">
             <div className="widget-header">
                 <div className="widget-title">
                     <i className={props.iconClass ? props.iconClass : ''}/> { props.title }
                 </div>
             </div>
+            </div>
+              <div className="widget-body-wrap">
             <div className={"widget-body " + (props.bodyClass ? props.bodyClass : '')}>
                { props.children }
+            </div>
             </div>
         </div>
 }
