bc7e510893be4ac5bd5f39be8f92e23cada184ae
Joshua Gagnon <josh.n.gagnon@gmail.com>
2017-06-14 09:15:15 +1200

    fixed postal migration

--- migrations/1497301164_postal_boolean.sql
+++ migrations/1497301164_postal_boolean.sql
@@ -1,1 +1,9 @@
-ALTER TABLE address_queries ADD COLUMN postal boolean default false;
+CREATE TABLE public.address_queries
+(
+  query character varying(255) NOT NULL,
+  postal boolean DEFAULT false,
+  addresses jsonb,
+  "createdAt" timestamp with time zone NOT NULL,
+  "updatedAt" timestamp with time zone NOT NULL,
+  CONSTRAINT address_queries_pkey PRIMARY KEY (query)
+)
